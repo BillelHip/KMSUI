0ccdeca6e603f261afc22dc3f76fe3b0352c697e
BilHip <barbaoui@gmail.com>
2016-11-08 19:24:52 +0800

    some things should change

--- KMS/pages/ManagePayment.php
+++ KMS/pages/ManagePayment.php
@@ -401,7 +401,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Create Student Profile</h1>
+                    <h1 class="page-header">Payment Report</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
