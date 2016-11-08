001c1bc59ae5a40edf822b419ab469d6e6891bda
BilHip <barbaoui@gmail.com>
2016-11-08 19:23:19 +0800

    some thinks should change

--- KMS/pages/Createteacherprofile.php
+++ KMS/pages/Createteacherprofile.php
@@ -410,7 +410,7 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Student information
+                            Teacher information
                         </div>
                         <div class="panel-body">
                             <div class="row">
