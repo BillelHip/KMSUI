03710bed42cf891b1e7f1af13318afd8066369b6
chaisit <chaisitc@gmail.com>
2016-12-24 20:46:47 +0700

    Update python setup.py and requirement.txt

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+import flask
+import tinydb
 
-import json
-
-app = Flask(__name__, static_folder='./KMS', template_folder='./KMS/templates',
-            static_url_path='', instance_relative_config=True)
+app = flask.Flask(__name__, static_folder='./KMS', template_folder='./KMS/templates',
+                  static_url_path='', instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 
 @app.route('/')
 def dashboard():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 @app.route('/login')
 @app.route('/login/<string:user>')
@@ -23,14 +22,14 @@ def login(user='teacher'):
         text['usertext'] = 'MyKidID'
         text['passwordtext'] = 'Parent IC No.'
 
-    return render_template('login.html', login=text)
+    return flask.render_template('login.html', login=text)
 
 @app.route('/userverify', methods=['POST', 'GET'])
 def user_verify():
-    req = request.form
+    req = flask.request.form
     ret = {'success': True}
     ret.update({'text': req['username']+' '+req['password']})
-    return jsonify(ret)
+    return flask.jsonify(ret)
 
 
 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='KMSUI',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='chaisit',
+    author_email='',
+    description=''
+)
